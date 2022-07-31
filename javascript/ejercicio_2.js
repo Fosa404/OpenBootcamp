a4889c8851a9b3bca4d06652f9c89a6ae90cc91d
Fosa404 <108636207+Fosa404@users.noreply.github.com>
2022-07-31 16:07:31 -0300

    Add files via upload

---
+++ javascript/ejercicio_2.js
@@ -0,0 +1,8 @@
+const me = ["Giuliano Fossatti", 31, false, new Date("july 24 1991"), Libro_fav={
+    titulo: "Tiempo de odio",
+    autor: "Andrzej Sapkowski",
+    fecha: new Date("1995"),
+    url: "https://www.amazon.com/-/es/Andrzej-Sapkowski/dp/8498890535"
+}];
+
+
