89985507c51a6230059c56f053d4322061a20bef
Giuliano Fossatti <108636207+Fosa404@users.noreply.github.com>
2022-08-04 17:45:03 -0300

    Add files via upload

---
+++ javascript/ejercicio4.js
@@ -0,0 +1,13 @@
+const heightCM = 169;
+const heightM = 1.69;
+const weight = 66.7;
+
+const heightRound = Math.ceil(heightM);
+
+
+
+const weigthRound = Math.floor(weight);
+
+
+const equal = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
