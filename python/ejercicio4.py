c93d0700706da57b1ebfcbd04e83e39be3fec487
Giuliano Fossatti <108636207+Fosa404@users.noreply.github.com>
2023-04-14 20:16:53 -0300

    Add files via upload

---
+++ python/ejercicio4.py
@@ -0,0 +1,13 @@
+num = 100
+
+while num > 0 :
+    print(num)
+    num -= 1
+
+
+for number in range(100, 0, -1):
+    print (num)
+
+while num > 0 :
+    print(num)
+    num -= 1
---
+++ python/prueba.py
@@ -0,0 +1,4 @@
+peso = float(input('tu peso '))
+altura = float(input('tu altura '))
+masa_corporal = round(peso / altura, 2)
+print(masa_corporal)
