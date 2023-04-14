f839fdc147831f12b098eb2255492c6edde8ccd0
Giuliano Fossatti <108636207+Fosa404@users.noreply.github.com>
2023-04-14 20:18:38 -0300

    Update ejercicio4.py

--- python/ejercicio4.py
+++ python/ejercicio4.py
@@ -1,13 +1,11 @@
+#usando while
 num = 100
 
 while num > 0 :
     print(num)
     num -= 1
 
-
+#usando for
+    
 for number in range(100, 0, -1):
     print (num)
-
-while num > 0 :
-    print(num)
-    num -= 1
