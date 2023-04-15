c8cac633c4be49a851212fe60b9a020a39d714a5
Giuliano Fossatti <108636207+Fosa404@users.noreply.github.com>
2023-04-15 16:26:57 -0300

    Add files via upload

--- python/ejercicio5.py
+++ python/ejercicio5.py
@@ -1,9 +1,12 @@
-def bisiesto(year):
-    if year > 365:
+anyo = int(input('Introduce un año'))
+
+print(anyo)
+
+def bisiesto():
+    if anyo % 4 == 0 and anyo % 100 == 0 and anyo % 400 == 0:
         print('El año es bisiesto')
 
     else:
         print('El año NO es bisiesto')
 
-bisiesto(366)
-
+bisiesto()
