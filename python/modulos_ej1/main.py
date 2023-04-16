2f770e14b7a7d6a05b9c4954ce69cfea65b211f4
Giuliano Fossatti <108636207+Fosa404@users.noreply.github.com>
2023-04-16 16:35:51 -0300

    Add files via upload

---
+++ python/modulos_ej1/main.py
@@ -0,0 +1,13 @@
+import calculator
+
+result_suma = calculator.suma(3,3)
+print(result_suma)
+
+result_resta = calculator.resta(5,2)
+print(result_resta)
+
+result_mult = calculator.multiplicacion(2,2)
+print(result_mult)
+
+result_div = calculator.division(10,2)
+print(result_div)
